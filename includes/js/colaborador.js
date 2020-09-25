8e41e418cc2bbc03787d8d31347b3457ad5f8ae1
apgoncalves <adalbpg@outlook.com>
2020-09-25 14:14:27 -0300

    Third Commit

--- includes/js/colaborador.js
+++ includes/js/colaborador.js
@@ -6,4 +6,22 @@ function carregaInfoColab() {
                 RACF: ${objUser.racf} <br><br>`
     document.getElementById("info").innerHTML = info;
     document.getElementById("imageUser").innerHTML = img;
+
+    var relatorio = "";
+
+    lista = objUser.listaOcorrencias;
+    for (i=0; i<lista.length; i++) {
+        var oc = lista[i];
+
+        relatorio = relatorio + 
+                    `
+                    <div class="row"> 
+                       <div class="col-4"><a href="justificativa_he.html?numSeq=${oc.numSeq}">${oc.dataOc}</a></div>
+                       <div class="col-3">${oc.numHoras}</div>
+                       <div class="col-4">${oc.atividade.nomeAtividade}</div>
+                       <div class="col-1">${oc.status}</div>
+                    </div>      
+                    `;
+    }
+    document.getElementById("relatorio").innerHTML = relatorio;
 }
--- includes/js/gestor.js
+++ includes/js/gestor.js
@@ -12,3 +12,43 @@ function logout() {
     localStorage.removeItem("userHE");
     location.href = "http://web.equipealfa.com.br/ProjetoFinal/index.html";
 }
+
+function buscarOCs() {
+    var status = document.getElementById("filtro").value;
+    var url = "http://web.equipealfa.com.br:8088/ocorrencias";
+    if(status == 1) {
+        url = url + "/status/1";
+    }
+    else if (status == 0) {
+        url = url + "/status/0";
+    }
+
+    fetch(url)
+        .then(res => res.json())
+        .then(lista => preencheLista(lista));
+}
+
+function preencheLista(lista){
+
+    console.log(lista);
+    var relatorio = "";
+
+    for (i=0; i<lista.length; i++) {
+        var oc = lista[i];
+        relatorio = relatorio + 
+                    `
+                    <div class="row"> 
+                       <div class="col-1">${oc.colaborador.racf}</div>
+                       <div class="col-2">${oc.colaborador.nomeUsuario}</div>
+                       <div class="col-1">${oc.dataOc}</div>
+                       <div class="col-1">${oc.numHoras}</div>
+                       <div class="col-2">${oc.atividade.nomeAtividade}</div>
+                       <div class="col-3">${oc.descricao}</div>
+                       <div class="col-1">${oc.pontoManual}</div>
+                       <div class="col-1">${oc.status}</div>
+                    </div>      
+                    `;
+
+    }
+    document.getElementById("relatorio").innerHTML = relatorio;
+}
